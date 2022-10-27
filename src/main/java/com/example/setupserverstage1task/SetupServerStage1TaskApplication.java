4b0b12eddf011b258fd0e64e988c59645e2f432a
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-10-27 21:29:22 +0100

    Intiial commit

---
+++ src/main/java/com/example/setupserverstage1task/SetupServerStage1TaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.setupserverstage1task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetupServerStage1TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetupServerStage1TaskApplication.class, args);
+    }
+
+}
