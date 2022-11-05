237fdd09e71c5ca7673aa1d979243f80b9a87fe2
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-05 23:19:48 +0100

    Update HngInternController.java

--- src/main/java/com/example/setupserverstage1task/infrastructure/controllers/HngInternController.java
+++ src/main/java/com/example/setupserverstage1task/infrastructure/controllers/HngInternController.java
@@ -1,7 +1,6 @@
 package com.example.setupserverstage1task.infrastructure.controllers;
 
 import com.example.setupserverstage1task.payload.response.HngInternProfileResponse;
-import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
