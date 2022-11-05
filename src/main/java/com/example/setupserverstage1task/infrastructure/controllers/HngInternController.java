03949e5339cf8f7147d1938ce095b789809d37c7
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-05 21:02:37 +0100

    Ready to deploy to Heroku

--- src/main/java/com/example/setupserverstage1task/infrastructure/controllers/HngInternController.java
+++ src/main/java/com/example/setupserverstage1task/infrastructure/controllers/HngInternController.java
@@ -1,6 +1,7 @@
 package com.example.setupserverstage1task.infrastructure.controllers;
 
 import com.example.setupserverstage1task.payload.response.HngInternProfileResponse;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HngInternController {
-    @GetMapping("/")
+
+
+    @GetMapping("/internProfile")
     public ResponseEntity<HngInternProfileResponse> getHngInternProfile() {
         return new ResponseEntity<>(new HngInternProfileResponse(
                 "Valleon",
                 true,
                 26,
-                "I'm a Java software Engineer, an Author, and an Actor."),
-                HttpStatus.OK);
+                "I'm a Java software Engineer, an Author, and an Actor."), HttpStatus.OK);
     }
+
+
+//   internProfile operation-calculation
 }
